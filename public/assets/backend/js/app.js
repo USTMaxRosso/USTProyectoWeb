06c9cfb5243dad280ea9c527f571bd7e925633f5
JMRosso <j.rosso@alumnos.santotomas.cl>
2024-06-30 21:01:05 -0400

    puedo agregar clientes =3

--- public/assets/backend/js/app.js
+++ public/assets/backend/js/app.js
@@ -1,30 +1,28 @@
 const formLogin = document.getElementById('formLogin');
-if(formLogin)
-{
+const formUpdate = document.getElementById('formUpdate');
+const formUpload = document.getElementById('formUpload'); 
+
+if (formLogin) {
     formLogin.addEventListener('submit', function(event) {
-        event.preventDefault(); // Evitar el envío del formulario por defecto
+        event.preventDefault();
         
-        // Obtener los datos del formulario
         const formData = new FormData(this);
        
-        // Convertir FormData a un objeto simple para depuración
         const plainFormData = Object.fromEntries(formData.entries());
         console.log('Datos del formulario:', plainFormData);
     
-        // Enviar los datos al servidor
         fetch('/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(plainFormData) // Enviar los datos como JSON
+            body: JSON.stringify(plainFormData) 
         })
         .then(response => {
             if (response.redirected) {
-                // Si la respuesta es una redirección, seguir la redirección
                 window.location.href = response.url;
             } else if (response.ok) {
-                return response.json(); // Si la respuesta es exitosa, convertir a JSON
+                return response.json(); 
             } else {
                 return response.json().then(data => {
                     throw new Error(data.message);
@@ -32,19 +30,16 @@ if(formLogin)
             }
         })
         .then(data => {
-            if (data) { // Asegurarse de que data no sea null
-                // Manejar la respuesta del servidor
+            if (data) { 
                 console.log('Respuesta del servidor:', data);
     
-                // Mostrar mensaje de error, si existe
                 if (data.message) {
                     document.getElementById('mensajeError').textContent = data.message;
                 }
             }
         })
         .catch(error => {
-            console.error('Error:', error); // Capturar y manejar errores generales
-            // Mostrar mensaje de error general en tu página HTML
+            console.error('Error:', error); 
             if (error.message) {
                 document.getElementById('mensajeError').textContent = error.message;
             }
@@ -52,3 +47,91 @@ if(formLogin)
     });
 }
 
+if (formUpdate) {
+    formUpdate.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        const formData = new FormData(this);
+
+        const plainFormData = Object.fromEntries(formData.entries());
+        console.log('Datos del formulario:', plainFormData);
+        fetch('/sitio-admin/modulo-editar-cliente', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(plainFormData) 
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else if (response.ok) {
+                return response.json(); 
+            } else {
+                return response.json().then(data => {
+                    throw new Error(data.message);
+                });
+            }
+        })
+        .then(data => {
+            if (data) { 
+                console.log('Respuesta del servidor:', data);
+    
+                if (data.message) {
+                    document.getElementById('mensajeError').textContent = data.message;
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            if (error.message) {
+                document.getElementById('mensajeError').textContent = error.message;
+            }
+        });
+    });
+}
+
+if (formUpload) {
+    formUpload.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        const formData = new FormData(this);
+       
+        const plainFormData = Object.fromEntries(formData.entries());
+        console.log('Datos del formulario:', plainFormData);
+        
+        fetch('/sitio-admin/modulo-ingresar-clientes', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(plainFormData) 
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else if (response.ok) {
+                return response.json(); 
+            } else {
+                return response.json().then(data => {
+                    throw new Error(data.message);
+                });
+            }
+        })
+        .then(data => {
+            if (data) { 
+                console.log('Respuesta del servidor:', data);
+    
+                if (data.message) {
+                    document.getElementById('mensajeError').textContent = data.message;
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error); 
+            if (error.message) {
+                document.getElementById('mensajeError').textContent = error.message;
+            }
+        });
+    });
+}
